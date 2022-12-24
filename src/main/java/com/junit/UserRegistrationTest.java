813f35777d4c0aef929a8b92bb15ccde08d98e7d
kapilmahajan123 <montyempire123gmail.com>
2022-12-24 09:45:40 +0530

    Added: Testing of Method Email

--- src/main/java/com/junit/UserRegistrationTest.java
+++ src/main/java/com/junit/UserRegistrationTest.java
@@ -35,5 +35,20 @@ public class UserRegistrationTest {
 	    	boolean result = userRegistration.lastName("mahajan");
 	    	Assert.assertEquals(false, result);
 	    }
+	    
+	    /*
+	     * User need to valid email id.
+	     */
+	    @Test
+		public void givenEmail_WhenProper_ShouldReturnTrue() {
+			boolean result = userRegistration.email("abc.xyz@bl.co.in");
+			Assert.assertEquals(true, result);
+		}
+
+		@Test
+		public void givenEmail_WhenNotProper_ShouldReturnFalse() {
+			boolean result = userRegistration.email("abc&.com");
+			Assert.assertEquals(false, result);
+		}
 
 }
