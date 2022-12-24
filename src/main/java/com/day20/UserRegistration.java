8a942a31bf4b45727e78b46ecbb232bf838ca60f
kapilmahajan123 <montyempire123gmail.com>
2022-12-24 14:46:23 +0530

    Added: JUnit test to validate user details

---
+++ src/main/java/com/day20/UserRegistration.java
@@ -0,0 +1,47 @@
+package com.day20;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Kapil
+ *
+ */
+public class UserRegistration {
+	
+	/*
+	 * uc-10
+	 * Junit Test to validate the user entry
+	 */
+	 private static final String NAME = "^[A-Z]{1}[a-z]{2,}$";
+	    private static final String EMAIL = "^(abc)[.][a-z]+[@](bl)[.](co)[.][a-z]+$";
+	    private static final String PHONE_NUMBER = "^[\\d]{2}[\\s][\\d]{10}$";
+	    private static final String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=[^!@#$%^&+=]*[!@#$%^&+=][^!@#$%^&+=]*$)(?=.*[0-9]).{8,}$";
+
+	    public boolean firstNameValidate(String fName)
+	    {
+	        return Pattern.matches(NAME, fName);
+	    }
+	    public boolean lastNameValidate(String lName)
+	    {
+	        return Pattern.matches(NAME, lName);
+	    }
+
+	    public boolean emailValidate(String email)
+	    {
+	        return Pattern.matches(EMAIL, email);
+	    }
+
+	    public boolean phoneNumberValidate(String phNum)
+	    {
+	        return Pattern.matches(PHONE_NUMBER, phNum);
+	    }
+
+	    public boolean passwordValidate(String pw)
+	    {
+	        return Pattern.matches(PASSWORD, pw);
+	    }
+
+}
---
+++ src/main/java/com/day20/UserRegistrationTest.java
@@ -0,0 +1,84 @@
+package com.day20;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+import junit.framework.Assert;
+
+/**
+ * 
+ * @author Kapil
+ *
+ */
+public class UserRegistrationTest {
+	
+	 @Test
+	    public void givenFirstName_whenProper_ShouldReturnTrue() {
+		 UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.firstNameValidate("Kapil");
+	        Assert.assertTrue(result);
+	    }
+
+	    @Test
+	    public void givenFirstName_whenNotProper_ShouldReturnFalse() {
+	    	UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.firstNameValidate("kapil");
+	        Assert.assertFalse(result);
+	    }
+
+	    @Test
+	    public void givenLastName_whenProper_ShouldReturnTrue() {
+	    	UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.lastNameValidate("Mahajan");
+	        Assert.assertTrue(result);
+	    }
+
+	    @Test
+	    public void givenLastName_whenNotProper_ShouldReturnFalse() {
+	    	UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.lastNameValidate("mahajan");
+	        Assert.assertFalse(result);
+	    }
+
+	    @Test
+	    public void givenEmail_whenProper_ShouldReturnTrue() {
+	    	UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.emailValidate("abc.xyz@bl.co.in");
+	        Assert.assertTrue(result);
+	    }
+
+	    @Test
+	    public void givenEmail_whenNotProper_ShouldReturnFalse() {
+	    	UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.emailValidate("abc.xyz@bl.in");
+	        Assert.assertFalse(result);
+	    }
+
+	    @Test
+	    public void givenPhone_whenProper_ShouldReturnTrue() {
+	    	UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.phoneNumberValidate("91 7879219494");
+	        Assert.assertTrue(result);
+	    }
+
+	    @Test
+	    public void givenPhone_whenNotProper_ShouldReturnFalse() {
+	    	UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.phoneNumberValidate("7879219494");
+	        Assert.assertFalse(result);
+	    }
+
+	    @Test
+	    public void givenPassword_whenProper_ShouldReturnTrue() {
+	    	UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.passwordValidate("Abc12345#");
+	        Assert.assertTrue(result);
+	    }
+
+	    @Test
+	    public void givenPassword_whenNotProper_ShouldReturnFalse() {
+	    	UserRegistration userRegistrationRegEx = new UserRegistration();
+	        boolean result = userRegistrationRegEx.passwordValidate("Shadab23#%");
+	        Assert.assertFalse(result);
+	    }
+
+}
