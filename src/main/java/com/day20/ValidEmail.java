eca1daf7dfcc29bf02942df15b2588a76e95d6b8
KapilMahajan123 <111445779+KapilMahajan123@users.noreply.github.com>
2022-12-30 09:27:21 +0530

    Added: Refactored code with Lambda Function

--- src/main/java/com/day20/ValidEmail.java
+++ src/main/java/com/day20/ValidEmail.java
@@ -2,8 +2,14 @@ package com.day20;
 
 import java.util.regex.Pattern;
 
+/**
+  *
+  *@author Kapil
+  *
+  **/
 public class ValidEmail
 {
+    //method to use valid emailId
     public boolean validateEmail(String email)
     {
         return Pattern.matches("^[\\w+_-]+(?:\\.[\\w+_-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$", email);
